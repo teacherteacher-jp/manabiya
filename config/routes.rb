fdbf189cd44550c59d56eaed5cef8539f9491f2b
juneboku <juneboku@gmail.com>
2024-06-25 21:12:26 +0900

    パスに名前をつける

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get "up", to: "rails/health#show", as: :rails_health_check
 
   get    "/auth/discord/callback", to: "sessions#create"
-  delete "/session",               to: "sessions#destroy"
+  delete "/session",               to: "sessions#destroy", as: "session"
   root   "root#index"
 end
