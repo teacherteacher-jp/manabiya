bd6141c1fabb9e54130985a0e3c039c1222547a2
juneboku <juneboku@gmail.com>
2024-06-25 21:25:25 +0900

    ApplicationControllerでログインチェックを行い、未ログインはGateに連れていく

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   include SessionHelper
+  before_action :redirect_to_gate_unless_logged_in
+
+  def redirect_to_gate_unless_logged_in
+    redirect_to gate_path unless logged_in?
+  end
 end
