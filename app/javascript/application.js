d00f5d4ca8a8f8bfdfe93597d831380555daaa6c
juneboku <juneboku@gmail.com>
2024-07-05 22:06:50 +0900

    フォームの見た目を整えたい気持ちはある

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll(".status-selection img").forEach(function(img) {
+    img.addEventListener("click", function() {
+      const status = this.dataset.status;
+      const date = this.dataset.date;
+      const hiddenField = document.getElementById(`${date}-status`);
+      hiddenField.value = status;
+
+      this.parentElement.querySelectorAll("img").forEach(function(sibling) {
+        sibling.classList.remove("selected");
+      });
+
+      this.classList.add("selected");
+    });
+  });
+});
