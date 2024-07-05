cf767fac5b410ed40d614e92e69e06a17035b2bb
juneboku <juneboku@gmail.com>
2024-07-05 17:33:06 +0900

    スケジュール入力は日付ごとのフォームがあるんじゃなく、全体でひとつのフォームに

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -5,9 +5,17 @@ class SchedulesController < ApplicationController
   end
 
   def create
-    schedule = Schedule.new(schedule_params)
-    schedule.member = current_member
-    schedule.save
+    schedules_params = params[:schedules].map do |schedule_params|
+      schedule_params.permit(:date, :status, :memo).tap do |sp|
+        sp[:status] = sp[:status].to_i if sp[:status].present?
+      end
+    end
+
+    schedules_params.each do |schedule_param|
+      next if schedule_param[:status].blank?
+      schedule = current_member.schedules.find_or_initialize_by(date: schedule_param[:date])
+      schedule.update(schedule_param)
+    end
 
     redirect_to schedules_path
   end
