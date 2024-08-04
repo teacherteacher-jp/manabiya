65e2aa488bf20a6f8572492f7a81018950aa1d81
juneboku <juneboku@gmail.com>
2024-08-04 17:11:30 +0900

    動作確認のため、一時的にOyasumi判定をオフにする

--- app/controllers/me_controller.rb
+++ app/controllers/me_controller.rb
@@ -1,5 +1,5 @@
 class MeController < ApplicationController
   def index
-    @dates = Date.today.upto(Date.today + 14.days).reject { Oyasumi.oyasumi?(_1) }
+    @dates = Date.today.upto(Date.today + 14.days)
   end
 end
