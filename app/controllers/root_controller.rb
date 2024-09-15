b1a3e5b734ed61320db8ec86d7c8b22d2518281f
juneboku <juneboku@gmail.com>
2024-09-15 22:24:41 +0900

    ビューに書いてあったデータをコントローラに移す

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,4 +1,9 @@
 class RootController < ApplicationController
   def index
+    @menu_items = [
+      { text: "わたしのスケジュール", path: me_path },
+      { text: "みんなのスケジュール", path: schedules_path },
+      { text: "みんなの居住地", path: regions_path },
+    ]
   end
 end
