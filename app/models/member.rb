0a37739ec68fe9c244e2f8b6131af4955e58a973
juneboku <juneboku@gmail.com>
2024-09-23 15:15:03 +0900

    Memberのserver_joined_atを取得して保存する処理を書く

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,4 +7,14 @@ class Member < ApplicationRecord
   validates :discord_uid, presence: true, uniqueness: true
   validates :name, presence: true, length: { maximum: 32 }
   validates :icon_url, presence: true, length: { maximum: 2083 }
+
+  after_save :fill_server_joined_at
+
+  def fill_server_joined_at
+    return if server_joined_at
+
+    bot = Discord::Bot.new(Rails.application.credentials.dig("discord_app", "bot_token"))
+    server_member = bot.server_member(discord_uid)
+    self.update_columns(server_joined_at: server_member.dig("joined_at"))
+  end
 end
