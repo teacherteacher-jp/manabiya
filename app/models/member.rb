bedf10be27738f288bb269470969b95edc8fa3d2
juneboku <juneboku@gmail.com>
2024-09-23 15:30:57 +0900

    Memberの「n期生」を算出するメソッドを実装する

--- app/models/member.rb
+++ app/models/member.rb
@@ -17,4 +17,15 @@ class Member < ApplicationRecord
     server_member = bot.server_member(discord_uid)
     self.update_columns(server_joined_at: server_member.dig("joined_at"))
   end
+
+  def generation
+    return nil unless server_joined_at
+
+    base_date = Date.new(2023, 12, 1)
+    return 1 if server_joined_at.to_date < base_date
+
+    joined_date = server_joined_at.to_date
+    months_difference = (joined_date.year * 12 + joined_date.month) - (base_date.year * 12 + base_date.month)
+    months_difference + 1
+  end
 end
