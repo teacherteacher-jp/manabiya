2d9b5b35607e064cfab08b6cb57cd4cecc1dd0c0
juneboku <juneboku@gmail.com>
2024-10-14 20:56:27 +0900

    FamilyMemberの編集機能をつくる

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,36 @@
 Rails.application.routes.draw do
   get "up", to: "rails/health#show", as: :rails_health_check
 
-  get    "/gate",                              to: "gate#index"
-  get    "/auth/discord/callback",             to: "sessions#create"
-  delete "/session",                           to: "sessions#destroy",
-                                               as: "session"
-  get    "/schedules",                         to: "schedules#index"
-  post   "/schedules",                         to: "schedules#create"
-  get    "/schedules/:date/assignments",       to: "assignments#index",
-                                               as: "schedule_assignments"
-  post   "/schedules/:schedule_id/assignment", to: "assignments#create",
-                                               as: "assignment"
-  delete "/schedules/:schedule_id/assignment", to: "assignments#destroy"
-  post   "/schedules/:date/notification",      to: "notifications#create",
-                                               as: "notification"
-  get    "/regions",                           to: "regions#index"
-  post   "/member_regions",                    to: "member_regions#create"
-  delete "/member_regions/:member_region_id",  to: "member_regions#destroy",
-                                               as: "member_region"
-  post   "/family_members",                    to: "family_members#create"
-  delete "/family_members/:family_member_id",  to: "family_members#destroy",
-                                               as: "family_member"
-  get    "/members/:member_id",                to: "members#show",
-                                               as: "member"
-  get    "/generations",                       to: "generations#index"
-  get    "/my/schedules",                      to: "my/schedules#index"
-  get    "/my/regions",                        to: "my/regions#index"
-  get    "/my/family_members",                 to: "my/family_members#index"
-  post   "/webhooks/metalife",                 to: "webhooks/metalife#create"
+  get    "/gate",                                  to: "gate#index"
+  get    "/auth/discord/callback",                 to: "sessions#create"
+  delete "/session",                               to: "sessions#destroy",
+                                                   as: "session"
+  get    "/schedules",                             to: "schedules#index"
+  post   "/schedules",                             to: "schedules#create"
+  get    "/schedules/:date/assignments",           to: "assignments#index",
+                                                   as: "schedule_assignments"
+  post   "/schedules/:schedule_id/assignment",     to: "assignments#create",
+                                                   as: "assignment"
+  delete "/schedules/:schedule_id/assignment",     to: "assignments#destroy"
+  post   "/schedules/:date/notification",          to: "notifications#create",
+                                                   as: "notification"
+  get    "/regions",                               to: "regions#index"
+  post   "/member_regions",                        to: "member_regions#create"
+  delete "/member_regions/:member_region_id",      to: "member_regions#destroy",
+                                                   as: "member_region"
+  post   "/family_members",                        to: "family_members#create"
+  delete "/family_members/:family_member_id",      to: "family_members#destroy",
+                                                   as: "family_member"
+  get    "/family_members/:family_member_id/edit", to: "family_members#edit",
+                                                   as: "edit_family_member"
+  patch  "/family_members/:family_member_id",      to: "family_members#update"
+  get    "/members/:member_id",                    to: "members#show",
+                                                   as: "member"
+  get    "/generations",                           to: "generations#index"
+  get    "/my/schedules",                          to: "my/schedules#index"
+  get    "/my/regions",                            to: "my/regions#index"
+  get    "/my/family_members",                     to: "my/family_members#index"
+  post   "/webhooks/metalife",                     to: "webhooks/metalife#create"
 
   root   "root#index"
 end
