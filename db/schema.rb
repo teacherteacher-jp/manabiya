9e4c3c63bd5098ee3792ba651e0ea5213043ab85
Saki-htr <saki.hattori0116@gmail.com>
2024-11-24 13:12:23 +0900

    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_10_135358) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_24_040557) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "assignments", force: :cascade do |t|
     t.bigint "schedule_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["schedule_id"], name: "index_assignments_on_schedule_id", unique: true
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description", null: false
+    t.datetime "start_at", null: false
+    t.string "venue", null: false
+    t.string "source_link", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["start_at"], name: "index_events_on_start_at"
   end
 
   create_table "family_members", force: :cascade do |t|
