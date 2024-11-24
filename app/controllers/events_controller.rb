7eac8fe0164370400ee82412eff9b10e3e16a0b1
Saki-htr <saki.hattori0116@gmail.com>
2024-11-24 18:49:42 +0900

    update: controllerとviewを実装

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,46 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
   def index
+    @events = Event.all
   end
 
   def show
   end
 
   def new
-  end
-
-  def edit
+    @event = Event.new
   end
 
   def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to @event, notice: 'イベントが作成されました'
+    else
+      render :new
+    end
   end
 
   def update
+    if @event.update(event_params)
+      redirect_to @event, notice: 'イベントが更新されました'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @event.destroy
+    redirect_to events_url, notice: 'イベントが削除されました'
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :start_at, :venue, :source_link)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
