b5d42c217b712fb7da83fae01e9e2c8af23b1851
Saki-htr <saki.hattori0116@gmail.com>
2024-11-24 19:24:43 +0900

    update: 入力必須と文字制限のバリデーション追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 50 }
+  validates :description, presence: true, length: { maximum: 200 }
+  validates :start_at, presence: true
+  validates :venue, presence: true, length: { maximum: 50 }
+  validates :source_link, presence: true
 end
