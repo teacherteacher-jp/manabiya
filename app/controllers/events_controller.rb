a5e8848763d3935c0f1b7335d799d4969cc25b8a
Saki-htr <saki.hattori0116@gmail.com>
2024-11-25 21:40:33 +0900

    update: 開催日時が近い順の方が使いやすい

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   def index
-    @events = Event.all
+    @events = Event.order(start_at: :asc)
   end
 
   def show
