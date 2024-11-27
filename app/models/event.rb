81c144943937bb654924edc15f0ca5955945dc72
juneboku <juneboku@gmail.com>
2024-11-27 21:13:19 +0900

    トップページに直近のイベント情報を載せる

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,6 @@ class Event < ApplicationRecord
   validates :start_at, presence: true
   validates :venue, presence: true, length: { maximum: 50 }
   validates :source_link, presence: true
+
+  scope :in_future, -> { where("start_at > ?", Time.current) }
 end
