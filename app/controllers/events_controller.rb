fed9d5118dec791f87d2af5d6938b94ee2548e6d
Saki-htr <saki.hattori0116@gmail.com>
2024-11-27 19:56:01 +0900

    delete: before_actionからも消す

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_event, only: [:show, :edit, :update]
 
   def index
     @events = Event.order(start_at: :asc)
