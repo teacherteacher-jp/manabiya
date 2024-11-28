9f86bd4b31d89ef83acd076e93622d055a22aab2
Saki-htr <saki.hattori0116@gmail.com>
2024-11-28 12:02:37 +0900

    fix: editアクションが無かった

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,6 +23,9 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def update
     if @event.update(event_params)
       redirect_to @event, notice: 'イベントが更新されました'
