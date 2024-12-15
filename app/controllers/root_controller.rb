c60faf8c5f3ba9a50a30a61f2b00a30eb8715f08
juneboku <juneboku@gmail.com>
2024-12-15 18:10:11 +0900

    みんなのスケジュールは誰でも確認できるようにする

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,14 +1,14 @@
 class RootController < ApplicationController
   def index
     @menu_items = [
-      { text: "わたしのプロフィール", path: member_path(current_member) },
       { text: "わたしのスケジュール", path: my_schedules_path },
+      { text: "みんなのスケジュール", path: schedules_path },
+      { text: "わたしのプロフィール", path: member_path(current_member) },
       { text: "みんなのゆかりの地", path: regions_path },
       { text: "みんなの参加時期", path: generations_path },
     ]
 
     @admin_menu_items = [
-      { text: "みんなのスケジュール", path: schedules_path },
     ]
 
     @recent_events = Event.in_future.order(start_at: :asc).limit(3)
