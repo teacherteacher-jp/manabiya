37accd3649535b534b4c569335bedeff9495d787
juneboku <juneboku@gmail.com>
2025-01-31 17:51:58 +0900

    MetaLifeのイベント通知におけるidとnameをロギングする

--- app/controllers/webhooks/metalife_controller.rb
+++ app/controllers/webhooks/metalife_controller.rb
@@ -3,9 +3,13 @@ class Webhooks::MetalifeController < WebhooksController
     content = params[:text]
     content += "\n<%s>" % [ENV["COMMUNITY_CENTER_URL"]] if content.include?("入室しました")
 
-    token = Rails.application.credentials.dig(:discord_app, :bot_token)
-    thread_id = Rails.application.credentials.dig(:discord, :community_center_thread_id)
-    pp Discord::Bot.new(token).send_message(channel_or_thread_id: thread_id, content:)
+    if params[:spaceId] == Rails.application.credentials.dig(:metalife, :community_center_space_id)
+      token = Rails.application.credentials.dig(:discord_app, :bot_token)
+      thread_id = Rails.application.credentials.dig(:discord, :community_center_thread_id)
+      pp Discord::Bot.new(token).send_message(channel_or_thread_id: thread_id, content:)
+    end
+
+    puts "{ space_id: '%s', session_id: '%s', name: '%s' }" % [params[:spaceId], params[:id], params[:name]]
 
     render json: { message: "ok" }, status: :ok
   end
