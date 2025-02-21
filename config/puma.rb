5418a1acf82b4bfc88440a594cbc71d913a5eb07
juneboku <juneboku@gmail.com>
2025-02-22 00:37:45 +0900

    Development環境ではPumaの中でジョブを処理してもらう

--- config/puma.rb
+++ config/puma.rb
@@ -34,7 +34,7 @@ port ENV.fetch("PORT", 3000)
 plugin :tmp_restart
 
 # Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] || Rails.env.development?
 
 plugin :tailwindcss if ENV.fetch("RAILS_ENV", "development") == "development"
 
