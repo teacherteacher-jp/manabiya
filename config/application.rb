e96a1425884cb006239ad783a24fb2048b923a87
juneboku <juneboku@gmail.com>
2025-02-22 00:34:24 +0900

    すべての環境でSolid Queueを使うぜ

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module Manabiya
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.time_zone = "Asia/Tokyo"
+
+    config.active_job.queue_adapter = :solid_queue
+    config.solid_queue.connects_to = { database: { writing: :queue } }
   end
 end
