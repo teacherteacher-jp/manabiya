0e6c316c9647d7fa9d92e8e768223134eb47632c
juneboku <juneboku@gmail.com>
2025-03-13 23:13:58 +0900

    メニューにStudentへのリンクを載せる

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,14 +1,12 @@
 class RootController < ApplicationController
   def index
     @menu_items = [
+      { text: "生徒さんたちの情報", path: students_path },
       { text: "わたしのスケジュール", path: my_schedules_path },
       { text: "みんなのスケジュール", path: schedules_path },
       { text: "わたしのプロフィール", path: member_path(current_member) },
       { text: "みんなのゆかりの地", path: regions_path },
       { text: "みんなの参加時期", path: generations_path },
-    ]
-
-    @admin_menu_items = [
     ]
 
     @recent_events = Event.in_future.order(start_at: :asc).limit(3)
