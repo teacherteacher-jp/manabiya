e6204a24a04ad31c7f9d6bfbce29b2708e08aa67
juneboku <juneboku@gmail.com>
2025-03-17 15:57:55 +0900

    トップページのメニューを整理する

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,9 +1,12 @@
 class RootController < ApplicationController
   def index
-    @menu_items = [
+    @school_menu_items = [
       { text: "生徒さんたちの情報", path: students_path },
-      { text: "わたしのスケジュール", path: my_schedules_path },
-      { text: "みんなのスケジュール", path: schedules_path },
+      { text: "ボランティアのスケジュール入力", path: my_schedules_path },
+      { text: "ボランティアのスケジュール確認", path: schedules_path },
+    ]
+
+    @community_menu_items = [
       { text: "わたしのプロフィール", path: member_path(current_member) },
       { text: "みんなのゆかりの地", path: regions_path },
       { text: "みんなの参加時期", path: generations_path },
